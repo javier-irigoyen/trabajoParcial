bfe182ae7e250c97117cf740aaa3b3d9a1c58f7c
Champux <86996746+Champux@users.noreply.github.com>
2023-01-28 19:36:00 -0500

    Update JefeDeZona.cpp

--- JefeDeZona.cpp
+++ JefeDeZona.cpp
@@ -1,12 +1,11 @@
 #include "JefeDeZona.h"
-
-JefeDeZona::JefeDeZona() : Empleado(){}
+JefeDeZona::JefeDeZona() : Empleado() {}
 JefeDeZona::JefeDeZona(string nombre, string apellidos,
 	string dni, string direccion,
 	string telefono, double salario, int aniosLaboral,
 	bool despacho, Secretario* secretario, Coche* carro, Vendedor** vendedores) : Empleado(nombre, apellidos, dni,
 		direccion, telefono, aniosLaboral, salario) {
-	if (despacho==true || despacho==false)
+	if (despacho == true || despacho == false)
 	{
 		this->despacho = despacho;
 	}
@@ -24,12 +23,12 @@ void JefeDeZona::eliminarVendedor(string dni) {}
 void JefeDeZona::imprimir() {
 	cout << endl;
 	cout << "Jefe de Zona" << endl;
-	cout << "============================================" << endl;
+	cout << "===========================================================" << endl;
 	Empleado::imprimir();
-	cout << "Despacho:\t\t" << (this->despacho ? "SI": "NO" )<< endl;
+	cout << "Despacho:                    \t" << (this->despacho ? "SI" : "NO") << endl;
 
-	cout << " Auto:" << endl;
-	cout << "  Matricula:\t\t" << this->carro->getMatricula() << endl;
-	cout << "  Marca:\t\t" << this->carro->getMarca() << endl;
-	cout << "  Modelo:\t\t" << this->carro->getModelo() << endl;
+	cout << "Auto:" << endl;
+	cout << "Matricula:                 \t" << this->carro->getMatricula() << endl;
+	cout << "Marca:                     \t" << this->carro->getMarca() << endl;
+	cout << "Modelo:                    \t" << this->carro->getModelo() << endl;
 }
